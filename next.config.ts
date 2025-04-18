8718d688dbe180eea91141beda4323be7fc3eb12
pro0o <pp48041721@student.ku.edu.np>
2025-04-18 18:56:39 +0545

    fix: redirect issue.

--- next.config.ts
+++ next.config.ts
@@ -18,32 +18,7 @@ const nextConfig: NextConfig = {
         pathname: "/a/09ir0bgwfb/**",
       },
     ],
-  },
-
- async redirects() {
-  return [
-    {
-      source: '/ai',
-      destination: 'https://sup-bud.ddns.net/',
-      permanent: false,
-    },
-    {
-      source: '/ai/:path*',
-      destination: 'https://sup-bud.ddns.net/:path*',
-      permanent: false,
-    },
-    {
-      source: '/raft-home',
-      destination: 'https://raft-in-motion.vercel.app/',
-      permanent: false,
-    },
-    {
-      source: '/raft-home/:path*',
-      destination: 'https://raft-in-motion.vercel.app/:path*',
-      permanent: false,
-    },
-  ]
-}
+  }
 }
 
 export default nextConfig
